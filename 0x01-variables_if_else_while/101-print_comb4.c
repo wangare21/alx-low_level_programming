87dcb81383477994e0b71903efadb8b02dd3ccc4
Wanagre21 <you@example.com>
2023-02-17 18:37:24 -0800

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of three digits
+ * using the smallest combination of three digits
+ *
+ * Return: 0 if program completes successfully
+ */
+int main(void)
+{
+int i, j, k;
+
+for (i = 0; i < 8; i++)
+{
+for (j = i + 1; j < 9; j++)
+{
+for (k = j + 1; k <= 9; k++)
+{
+putchar(i + '0');
+putchar(j + '0');
+putchar(k + '0');
+
+if (i != 7 || j != 8 || k != 9)
+{
+putchar(',');
+putchar(' ')
+}
+}
+}
+}
+printf("\n");
+
+return (0);
+}
