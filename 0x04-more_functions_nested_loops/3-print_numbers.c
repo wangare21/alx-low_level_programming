67ecedbc9a60d03264040b40f225a42903c5fad8
Your Name <you@example.com>
2023-02-23 08:00:31 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,16 +2,17 @@
 
 /**
  * print_numbers - Print the numbers since 0 up to 9
+ *
  * Return: The numbers since 0 up to 9
  */
 
-void print_numbers(void)
+void print_numbers(void);
 {
 	int x;
 
 	for (x = 0; x <= 9; x++)
 	{
-	_putchar(x + '0');
+		_putchar(x + '0');
 	}
 	_putchar('\n');
 }
