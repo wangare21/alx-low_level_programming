1c2924508954957de883308c3844db704c4546ce
Your Name <you@example.com>
2023-03-02 08:14:35 -0800

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -14,14 +14,13 @@ char *leet(char *n)
 
 	for (i = 0; n[i] != '\0'; i++)
 	{
-		for (j = 0; j < 10; j++)
-		{
-			if (n[i] == s1[j])
-			{
-				n[i] = s2[j];
-			}
-		}
+	for (j = 0; j < 10; j++)
+	{
+	if (n[i] == s1[j])
+	{
+	n[i] = s2[j];
+	}
+	}
 	}
 	return (n);
 }
-
