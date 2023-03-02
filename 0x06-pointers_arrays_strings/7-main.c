8ea536bcaa4755debd495f7823c22c1627543fa1
Your Name <you@example.com>
2023-03-02 08:11:53 -0800

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char s[] = "Expect the best prepare for the worst capitalize on what comes\n";
+	char *p;
+
+	p = leet(s);
+	printf("%s", p);
+	printf("%s", s);
+	return (0);
+}
