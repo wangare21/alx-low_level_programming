8d17ef7b0100d504d6fa1210275f6fc48e3da359
Your Name <you@example.com>
2023-03-10 06:16:36 -0800

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - prints the name of the program
@@ -9,7 +10,7 @@
 
 int main(int argc, char *argv[])
 {
-	printf("%s\n"' *argv[0]);
+	printf("%s\n", argv[0]);
 	(void)argc;
 	return (0);
 }
