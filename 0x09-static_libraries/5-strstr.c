1f03a10147bcc64d15a3309c18652358271fd873
Your Name <you@example.com>
2023-03-10 10:44:14 -0800

    libmy.a, main.h

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Entry
+ * @a: array
+ * Return: Always 0 (Success)
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 8; i++)
+	{
+	_putchar(a[i][j]);
+	_putchar('\n');
+	}
+}
