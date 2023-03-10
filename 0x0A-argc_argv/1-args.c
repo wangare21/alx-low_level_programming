eb06f7837a9752cbff6d35017903f8c8cf1a6b1e
Your Name <you@example.com>
2023-03-10 06:26:42 -0800

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - write a program that prints the number of arguments passed on it
+ * @args: This is the argument count
+ * @argv: This is the argument vector
+ * Return: This return to 0
+ */
+
+int main(int args, char *argv[])
+{
+	printf("%d\n", args - 1);
+	(void)argv;
+	return (0);
+}
+
