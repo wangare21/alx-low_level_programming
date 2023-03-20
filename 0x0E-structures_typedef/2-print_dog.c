81f60d97559b77317d75b7260e107538e1d2af19
Wangare21 <you@example.com>
2023-03-20 08:30:33 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,6 +13,8 @@ void print_dog(struct dog *d)
 		return;
 	if (d->name == NULL)
 		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
 	if (d->age < 0)
 		printf("Age: (nil)\n");
 	else
