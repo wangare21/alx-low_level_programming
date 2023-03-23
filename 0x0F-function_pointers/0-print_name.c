40f9d21604974ed96a2a5dc594e7ac01506146f6
Wangare21 <you@example.com>
2023-03-23 04:55:52 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,11 +5,12 @@
  * print_name - prints a name
  *@name: name to print
  *@f: pointer to the printing function
+ *Return: nothing
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (!name || !f)
+	if (name == NULL || f == NULL)
 		return;
 	f(name);
 }
