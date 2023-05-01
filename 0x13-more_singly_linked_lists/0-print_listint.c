f5504e07e1b1adf9de1bcc1d931293c39eea1e15
Wangare21 <you@example.com>
2023-05-01 14:45:12 -0700

    task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,15 +10,15 @@
 size_t print_listint(const listint_t *h)
 
 {
-	size_t num = 0;
+	size_t nodes = 0;
 
 	while (h)
 	{
 	
-		num++;
+		nodes++;
 		printf("%d\n", h->n);
 		h = h->next;
 	}
 
-	return (num);
+	return(nodes);
 }
