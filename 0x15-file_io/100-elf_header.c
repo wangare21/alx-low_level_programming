78be3969e1fb0bd0db1913cb939a6bb1991c029a
Wangare21 <you@example.com>
2023-05-12 03:02:45 -0700

    task 3

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -8,7 +8,7 @@
 
 void print_type(unsigned int e_type, unsigned char *e_ident);
 void print_entry(unsigned long int e_entry, unsigned char *e_ident);
-void close_elf(int elf)
+void close_elf(int elf);
 void check_elf(unsigned char *e_ident);
 void print_magic(unsigned char *e_ident);
 void print_class(unsigned char *e_ident);
@@ -23,6 +23,7 @@ void print_osabi(unsigned char *e_ident);
  *
  * Description: if the file is not an ELF file - exit code 98.
  */
+
 void check_elf(unsigned char *e_ident)
 {
 	int index;
@@ -46,6 +47,7 @@ void check_elf(unsigned char *e_ident)
  *
  * Description: The magic numbers are separated by spaces.
  */
+
 void print_magic(unsigned char *e_ident)
 {
 	int index;
@@ -67,6 +69,7 @@ void print_magic(unsigned char *e_ident)
  * print_class - It prints the class of an ELF header.
  * @e_ident: A pointer to an array containing the ELF class.
  */
+
 void print_class(unsigned char *e_ident)
 {
 	printf(" Class: ");
@@ -91,6 +94,7 @@ void print_class(unsigned char *e_ident)
  * print_data - It prints the data of an ELF header.
  * @e_ident: A pointer to an array containing the ELF class.
  */
+
 void print_data(unsigned char *e_ident)
 {
 	printf(" Data: ");
@@ -180,6 +184,7 @@ void print_osabi(unsigned char *e_ident)
  * print_abi - It Prints the ABI version of an ELF header.
  * @e_ident: A pointer to an array containing the ELF ABI version.
  */
+
 void print_abi(unsigned char *e_ident)
 {
 	printf(" ABI Version: %d\n",
@@ -191,6 +196,7 @@ void print_abi(unsigned char *e_ident)
  * @e_type: the ELF type.
  * @e_ident: A pointer to an array containing the ELF class.
  */
+
 void print_type(unsigned int e_type, unsigned char *e_ident)
 {
 	if (e_ident[EI_DATA] == ELFDATA2MSB)
@@ -300,7 +306,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	print_data(header->e_ident);
 	print_version(header->e_ident);
 	print_osabi(header->e_ident);
-	print_abi(header->e_ident)
+	print_abi(header->e_ident);
 	print_magic(header->e_ident);
 	print_class(header->e_ident);
 	print_type(header->e_type, header->e_ident);
@@ -308,7 +314,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	check_elf(header->e_ident);
 	printf("ELF Header:\n");
 
-	ree(header);
+	free(header);
 	close_elf(o);
 	return (0);
-	}
+}
